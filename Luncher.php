bc40156cabb9a3bab94e4551e997d90a64960022
GrandFather051 <mr.focker42@gmail.com>
2016-12-25 02:25:46 +0330

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define('BOT_TOKEN', '322142695:AAH3MYoD12yGkWQQeWRFkgFGxj3G23WxeZo');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
